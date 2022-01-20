64ab5abe773294aa5512aebc0ac6171fe1b3535a
sean <shonum@gmail.com>
2022-01-20 18:19:34 +0800

    auth popup ui

--- apps/extension/src/components/AuthPopup.tsx
+++ apps/extension/src/components/AuthPopup.tsx
@@ -1,11 +1,44 @@
-import React from 'react';
+import React, { useEffect, SyntheticEvent } from 'react';
+import { PrimaryButtonWide, WhiteButtonWide } from 'ui';
+
+const webUrl = 'http://localhost:3001';
 
 export const AuthPopup: React.FC = () => {
+  useSetPopupDimensions();
+  const goTo = (url: string) => (e: SyntheticEvent) => {
+    e.preventDefault();
+    const newWindow = window.open(url, '_blank', 'noopener,noreferrer');
+    if (newWindow) newWindow.opener(null);
+  };
   return (
-    <div className="p-6">
-      <header className="font-medium text-xl">
-        <p className="text-gray-500">Not authenticatedzzz</p>
-      </header>
+    <div className="min-h-full flex flex-col justify-center py-16 px-16">
+      <div className="sm:mx-auto sm:w-full sm:max-w-md">
+        <img
+          className="mx-auto h-12 w-auto"
+          src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg"
+          alt="Workflow"
+        />
+        <h2 className="mt-6 mb-12 text-center text-xl font-semibold text-gray-900 leading-7">
+          Welcome to CBE
+        </h2>
+        <PrimaryButtonWide onClick={goTo(webUrl + '/login')}>
+          Sign in
+        </PrimaryButtonWide>
+        <WhiteButtonWide onClick={goTo(webUrl + '/signup')} classes="mt-4">
+          Sign up
+        </WhiteButtonWide>
+      </div>
     </div>
   );
 };
+
+function useSetPopupDimensions() {
+  useEffect(() => {
+    const $body = document.querySelector('body');
+    // visible width can sometimes be less than 800px
+    if ($body) {
+      $body.style.width = `${Math.min(window.innerWidth, 440)}px`;
+      $body.style.height = `${400}px`;
+    }
+  }, []);
+}
--- packages/ui/components/Buttons.tsx
+++ packages/ui/components/Buttons.tsx
@@ -11,6 +11,8 @@ const WIDE_DEFAULTS =
   "w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium";
 const PRIMARY_DEFAULTS =
   "text-white border-transparent text-white bg-blue-600 hover:bg-blue-700";
+const WHITE_DEFAULTS =
+  "border-gray-300 shadow-sm text-gray-700 bg-white hover:bg-gray-50";
 
 export const PrimaryButtonWide = ({ ...props }) => (
   <button
@@ -25,6 +27,19 @@ export const PrimaryButtonWide = ({ ...props }) => (
   />
 );
 
+export const WhiteButtonWide = ({ ...props }) => (
+  <button
+    type="button"
+    className={classNames(
+      WIDE_DEFAULTS,
+      WHITE_DEFAULTS,
+      "focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500",
+      props.classes
+    )}
+    {...props}
+  />
+);
+
 export const PrimaryButtonSmall = ({ ...props }) => (
   <button
     type="button"
@@ -32,7 +47,7 @@ export const PrimaryButtonSmall = ({ ...props }) => (
       BUTTON_DEFAULTS,
       SMALL_DEFAULTS,
       PRIMARY_DEFAULTS,
-      "shadow-sm ",
+      "shadow-sm",
       props.classes
     )}
     {...props}
@@ -58,7 +73,7 @@ export const WhiteButtonSmall = ({ ...props }) => (
     className={classNames(
       BUTTON_DEFAULTS,
       SMALL_DEFAULTS,
-      "border-gray-300 shadow-sm text-gray-700 bg-white hover:bg-gray-50",
+      WHITE_DEFAULTS,
       props.classes
     )}
     {...props}
