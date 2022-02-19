bbf7dddd60fae994f805a667003e3b2f4a69a6a3
sean <shonum@gmail.com>
2022-02-19 10:58:22 +0800

    s: fix the main body style

--- apps/extension/src/components/Dashboard/Main.tsx
+++ apps/extension/src/components/Dashboard/Main.tsx
@@ -13,7 +13,7 @@ export default function Main() {
   const { collections, activeIdx } = useDashStore();
   const collection = collections[activeIdx];
   return (
-    <div className="pl-2 pr-3 max-w-full">
+    <div className="pl-2 max-w-full">
       {!collection ? (
         <NoCollectionSelected />
       ) : (
@@ -63,7 +63,7 @@ function Header({ collectionName }: HeaderProps) {
 
   return (
     <>
-      <div className="flex-none flex pt-4 w-full mb-4">
+      <div className="flex-none flex pt-4 w-full mb-4 pr-3">
         <div className="flex flex-1 items-center pr-2">
           <FolderOpenIcon className="w-6 mr-2 flex-none" />
           <div className="flex-auto w-32 text-lg leading-7 font-medium truncate">
@@ -103,7 +103,7 @@ function Body({ links }: { links: Link[] }) {
   }, []);
 
   return (
-    <div className="flex flex-col flex-1 max-h-[436px] overflow-y-auto space-y-2">
+    <div className="flex flex-col flex-1 max-h-[538px] overflow-y-auto space-y-2">
       {links?.map((link, i) => (
         <div key={link.id} className="flex bg-white rounded-md p-2">
           <div>icon</div>
